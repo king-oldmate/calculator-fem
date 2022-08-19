b510fdbed1e0fe01458638f80c9b8c821dac861e
king-oldmate <ray.z@live.com.au>
2022-08-19 20:59:35 +1000

    Added components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import React from 'react'
+import useLocalStorage from 'use-local-storage';
+import './App.css'
 
 function App() {
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light')
+
+const switchTheme = () => {
+  const newTheme = theme === 'light' ? 'dark' : 'light'
+  setTheme(newTheme)
+}
+
   return (
-    <div className="App">
-app
+    <div className="App" data-theme={theme}>
+      app
+      <button onClick={switchTheme}>Switch to {theme === 'light' ? 'Dark' : 'Light'}</button>
     </div>
   );
 }
